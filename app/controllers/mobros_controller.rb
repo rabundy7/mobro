3f61a74f652055bd78c5a951699ee5e56f3b5a5b
Alan Bundy <rabundy7@yahoo.com>
2010-06-05 15:05:08 -0400

    added link for creating a column

--- app/controllers/mobros_controller.rb
+++ app/controllers/mobros_controller.rb
@@ -10,20 +10,26 @@ class MobrosController < ApplicationController
     assoc_types.each{|t| @assocs_hash[t] = assocs.select{|a| a.macro == t} || []}
   end
   
-  def add_column
+  def new_column
+    @model = Object.const_get(params[:model_name])
+#    puts "AAAAAAAAAAAAAAAAAAA"
+#    render :template => 'mobros/new_column.js.rjs', :locals => {:model => Object.const_get(params[:model])}
+  end
+  
+  def create_column
     # Generate migration
     %x[ruby script/generate migration add_#{params[:column_name]}_to_#{params[:model].underscore} #{params[:column_name]}:#{params[:column_type]}]
     # Run migration
     %x[rake db:migrate]
-    render :template => 'mobros/add_column.js.rjs', :locals => {:model => Object.const_get(params[:model].titleize)}
+    render :template => 'mobros/create_column.js.rjs', :locals => {:model => Object.const_get(params[:model].titleize)}
   end
   
-  def add_model
+  def create_model
     # Generate migration
     %x[ruby script/generate model #{params[:model_name].underscore}]
     # Run migration
     %x[rake db:migrate]
-    render :template => 'mobros/add_model.js.rjs', :locals => {:model => Object.const_get(params[:model_name].titleize)}
+    render :template => 'mobros/create_model.js.rjs', :locals => {:model => Object.const_get(params[:model_name].titleize)}
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :mobros
-  map.add_model_inspectors_column 'mobros/add_column', :controller => 'model_inspectors', :action => 'add_column'
-  map.add_model_inspectors_model 'mobros/add_model', :controller => 'model_inspectors', :action => 'add_model'
+  map.new_mobros_column 'mobros/new_column', :controller => 'mobros', :action => 'new_column'
+  map.create_mobros_column 'mobros/create_column', :controller => 'mobros', :action => 'create_column'
+  map.new_mobros_model 'mobros/new_model', :controller => 'mobros', :action => 'new_model'
+  map.create_mobros_model 'mobros/create_model', :controller => 'mobros', :action => 'create_model'
 end
